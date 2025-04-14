fdad0b364996a57f310d636d6c047eb89e0f318c
Mykhailo Mykhalov <mykhailo.mykhalov@axel.org>
2025-04-14 10:46:29 +0300

    Update config routes to be compatible of Spree.admin_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.add_routes do
-  namespace :admin do
+  namespace :admin, path: Spree.admin_path do
     resource :active_shipping_settings, only: [:show, :update, :edit]
 
     resources :products, only: [] do
